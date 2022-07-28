5de420819ed31591027b35fb017de4e103d17096
Sa238 <108183566+Sa238@users.noreply.github.com>
2022-07-28 20:09:18 +0530

    Day-3 Assignment submitted

---
+++ main.js
@@ -0,0 +1,18 @@
+let names = ["suarabh", "Sachin", "Suman", "Surbhi", "Bittu"];
+let reverseNames = names.reverse()
+// console.log(reverseNames);
+console.log(names);
+
+
+
+
+let country= {
+    name : "india",
+    capital : "Bhopal",
+    population : 138,
+
+    
+
+}
+
+console.log(country);
